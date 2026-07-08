c2ae25ca1fe3d75defc1a6d1429e9fdc9af33774
Kennely Ray <krbucang@gmail.com>
2026-07-08 08:38:58 +0800

    Change My Portfolio to Ken Dev Portfolio

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: "My Portfolio | Web Developer",
+  title: "Ken-Dev Portfolio | Web Developer",
   description: "Web Developer Portfolio inspired by Lando Norris website",
 };
 
